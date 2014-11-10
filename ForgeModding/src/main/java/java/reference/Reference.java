bcdb48ec6afc60c0ced3825c57f259198495616f
LordfelmetII <lordfelmetiii@gmail.com>
2014-11-10 10:21:33 -0500

    Update Reference.java

--- ForgeModding/src/main/java/java/reference/Reference.java
+++ ForgeModding/src/main/java/java/reference/Reference.java
@@ -4,6 +4,6 @@ public class Reference
 {
     public static final String MOD_ID = "NP0.0";
     public static final String MOD_NAME = "Newtonian Physivs";
-    public static final String VERSION = "@Version@";
+    public static final String VERSION = "1.7.10";
  
 }
