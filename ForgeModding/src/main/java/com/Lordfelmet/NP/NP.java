d9c82fe30e3961849deb631bbea94baf25a6772f
lordfelmetII <lordfelmetiii@gmail.com>
2014-11-24 18:25:32 -0500

    Setup Fixes
    I swear Ill add content some day

--- ForgeModding/src/main/java/com/Lordfelmet/NP/NP.java
+++ ForgeModding/src/main/java/com/Lordfelmet/NP/NP.java
@@ -2,14 +2,22 @@ package com.Lordfelmet.NP;
 
 
 import java.reference.Reference;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockContainer;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class NP
 {
+	//  @SidedProxy(clientSide="com.Lordfelmet.Proxies.ClientProxy", serverSide="com.Lordfelmet.Proxies.CommonProxy")
+    
+	  
     @Mod.Instance(Reference.MOD_ID)
     public static NP instance;
 
@@ -19,13 +27,16 @@ public class NP
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-       
+    	 
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        
+    	//Block test1 = new Test1();
+    	//proxy.registerRenderThings();
+        //GameRegistry.registerTileEntity(TileEntityTest1.class, test1.getLocalizedName());
+        //GameRegistry.registerBlock(test1, test1.getLocalizedName());
     }
 
     @Mod.EventHandler
